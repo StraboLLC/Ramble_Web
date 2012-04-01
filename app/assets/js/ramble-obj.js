9a9937de605b17f06b633c3e9633a543967570a1
Will Potter <will@wpotter.me>
2012-04-01 14:40:17 -0400

    worked on new library and added jsdoc

--- app/assets/js/ramble-obj.js
+++ app/assets/js/ramble-obj.js
@@ -1,87 +1,318 @@
-function Track(filename,json,map) {
-	this.filename=filename;
+// build/jsdoc/jsdoc app/assets/js/ramble-obj.js -d build/doc
+/* Global Variables */
+var map;
+/**
+ * A Ramble Track.
+ * @constructor
+ */
+function Track(name,json,map) {
+	this.name=name;
 	this.points=json.track.track.points;
 	this.map=map;
 	this.strokeColor="rgb(48,157,230)";
 	this.strokeOpacity=0.73;
 	this.strokeWeight=5;
 	this.currentPoint=0;
-}
-Track.prototype.initialize = function() {
+	this.listItem = document.createElement('div');
 
-	this.points=tracks[idx].track.track.points;
 	this.routeCoords = [];
-	var llb = new google.maps.LatLngBounds();
 	for(var x in this.points) {
 		this.routeCoords.push(new google.maps.LatLng(this.points[x].latitude,this.points[x].longitude));
 	}
-	llb.extend(new google.maps.LatLng(this.points[0].latitude,this.points[0].longitude));
-	this.map.fitBounds(llb);
-	this.currentRoute = new google.maps.Polyline({
+	latLngBounds.extend(new google.maps.LatLng(this.points[0].latitude,this.points[0].longitude));
+	map.map.fitBounds(latLngBounds);
+	this.route = new google.maps.Polyline({
 		path: this.routeCoords,
-		strokeColor: this.strokeColor,
-		strokeOpacity: this.strokeOpacity,
-		strokeWeight: this.strokeWeight
+		strokeColor: "rgb(48,157,230)",
+		strokeOpacity: 0.73,
+		strokeWeight: 5
 	});
-	this.currentRoute.setMap(this.map);
-	var deg = Math.round(this.points[0].heading)-90;
-	this.richMarker = new RichMarker({
-		map: this.map,
+	this.route.setMap(map.map);
+	this.deg = Math.round(this.points[0].heading)-90;
+	this.marker = new RichMarker({
+		map: map.map,
 		flat: true,
 		position: new google.maps.LatLng(this.points[0].latitude,this.points[0].longitude),
 		anchor: RichMarkerPosition.MIDDLE,
-		content: '<div class="marker"><div id="'+idx+'-marker" class="video-camera-marker" style="transform:rotate('+deg+'deg);-moz-transform:rotate('+deg+'deg);-webkit-transform:rotate('+deg+'deg);-o-transform:rotate('+deg+'deg);-ms-transform:rotate('+deg+'deg);" ><img src="/app/assets/images/arrow.png" alt="" /></div><div class="tooltip"><img src="//graph.facebook.com/'+tracks[idx].user_id+'/picture" alt="" title="'+tracks[idx].name+'"/></div></div>'
+		content: '<div class="marker"><div id="'+name.filename+'-marker" class="video-camera-marker" style="transform:rotate('+this.deg+'deg);-moz-transform:rotate('+this.deg+'deg);-webkit-transform:rotate('+this.deg+'deg);-o-transform:rotate('+this.deg+'deg);-ms-transform:rotate('+this.deg+'deg);" ><img src="/app/assets/images/arrow.png" alt="" /></div><div class="tooltip"><img src="//graph.facebook.com/'+this.user_id+'/picture" alt="" title="'+this.name+'"/></div></div>'
 	});
-	this.richMarker.setMap(this.map);
-	google.maps.event.addListener(this.richMarker, 'click', function() {
-		cT = idx;
-		initViewer(tracks[idx].filename);
+	this.marker.setMap(map.map);
+	google.maps.event.addListener(this.marker, 'click', function() {
+		initViewer(this.filename);
 		var llb = new google.maps.LatLngBounds();
-		for(var x in tracks[idx].points) {
-			llb.extend(new google.maps.LatLng(tracks[idx].points[x].latitude,tracks[idx].points[x].longitude));
+		for(var x in this.points) {
+			llb.extend(new google.maps.LatLng(this.points[x].latitude,this.points[x].longitude));
 		}
-		map.fitBounds(llb);
+		map.map.fitBounds(llb);
 	});
+}
+/**
+ * Shows the track Marker and Route on its Map object.
+ */
+Track.prototype.show=function() {
+	this.marker.setMap(this.map.map);
+	this.route.setMap(this.map.map);
 };
-Track.prototype.initViewer = function(video_element) {
-	$('#scrub-bar').currentTrack=this;
-	video_element.currentTrack=this;
-	currentPoint=0;
-	document.getElementById('video_container').style.height="368px";
-	document.getElementById('played').style.width="0px";
-	if(video_element.canPlayType("video/webm")) video.src="http://s3.amazonaws.com/ramble/"+this.filename+"/"+this.filename+".webm";
-	else if(video_element.canPlayType("video/mp4")) video.src="http://s3.amazonaws.com/ramble/"+this.filename+"/"+this.filename+".mp4";
-	else video_element.innerHTML = "Sorry, your browser does not support HTML5 Video. Please try using a compatible browser. We recommend <a href=\"http://chrome.google.com\">Google Chrome</a>.";
-	document.getElementById('video-title').innerHTML = tracks[cT].name;
-	//video_element.currentTime=0;
-	document.getElementById('play-pause').onclick = function() {
-		if(video_element.paused===true) {
-			play();
-			this.style.background = "url('/app/assets/images/pause.png') center center no-repeat";
-		} else {
-			pause();
-			this.style.background = "url('/app/assets/images/play.png') center center no-repeat";
+/**
+ * Hides the track Marker and Route from its Map object.
+ */
+Track.prototype.hide=function() {
+	this.marker.setMap(null);
+	this.route.setMap(null);
+};
+/**
+ * Shows the track Route on its Map object.
+ */
+Track.prototype.showRoute=function() {
+	this.route.setMap(this.map.map);
+};
+/**
+ * Hides the track Route from its Map object.
+ */
+Track.prototype.hideRoute=function() {
+	this.route.setMap(null);
+};
+/**
+ * Queries the server to delete the Track from the database.
+ */
+Track.prototype.deleteTrack=function() {
+	var fd = new FormData();
+	fd.append('filename',this.name);
+	var oXHR = new XMLHttpRequest();
+	oXHR.open("POST", "/api/index.php?delete_video");
+	oXHR.onreadystatechange = function (oEvent) {
+		if (oXHR.readyState === 4) {
+			if (oXHR.status === 200) {
+				//Do on completion
+			} else {
+				console.log("Error", oXHR.statusText);
+			}
 		}
 	};
-	$('#scrub-bar').click(function(event) {
-		var pos = findPos(this);
-		var a = (event.pageX-pos.x);
-		document.getElementById('played').style.width=a+"px";
-		var percentDone = a/(convertCssPxToInt(document.getElementById('video_container').style.width)-60);
-		video.currentTime=percentDone*video.duration;
-		var accuratePoint=0;
-		a=10;
-		var l=10;
-		for(var x in this.currentTrack.points) {
-			a=Math.abs( this.currentTrack.points[x].timestamp-(percentDone*video.duration));
-			//console.log(a);
-			if(a<l){l=a;accuratePoint=x;}
-		}
+	oXHR.send(fd);
+};
+/**
+ * A list of Ramble Tracks.
+ * @constructor
+ */
+function TrackList(elt) {
+	this.elt=elt;
+	this.count=0;
+	this.data=[];
+}
+/**
+ * Adds a Track object as the last item in the list.
+ * @param {Track}
+ */
+TrackList.prototype.add=function(track) {
+	this.count=this.data.push(track);
+};
+/**
+ * Removes an element from the list at the given index.
+ * @param {number}
+ */
+TrackList.prototype.remove=function(index) {
+	return this.data.splice(index,1);
+};
+/**
+ * Shows all tracks in the list on their respective maps.
+ */
+TrackList.prototype.show=function() {
+	for(var x in this.data) {
+		this.data[x].show();
+	}
+};
+/**
+ * Hides all tracks in the list on their respective maps.
+ */
+TrackList.prototype.hide=function() {
+	for(var x in this.data) {
+		this.data[x].hide();
+	}
+};
+/**
+ * Shows all track routes in the list on their respective maps.
+ */
+TrackList.prototype.showRoutes=function() {
+	for(var x in this.data) {
+		this.data[x].showRoute();
+	}
+};
+/**
+ * Hides all track routes in the list on their respective maps.
+ */
+TrackList.prototype.hideRoutes=function() {
+	for(var x in this.data) {
+		this.data[x].hideRoute();
+	}
+};
+/**
+ * Shows all track markers in the list on their respective maps.
+ */
+TrackList.prototype.showMarkers=function() {
+	for(var x in this.data) {
+		this.data[x].showMarker();
+	}
+};
+/**
+ * Hides all track markers in the list on their respective maps.
+ */
+TrackList.prototype.hideMarkers=function() {
+	for(var x in this.data) {
+		this.data[x].hideMarker();
+	}
+};
+/**
+ * The Map that builds upon a Google Maps Map.
+ * @constructor
+ */
+function Map(elt) {
+	this.elt=elt;
+	var mapOptions = {
+		center: new google.maps.LatLng(44.00858496025453,286.8300942840576),
+		zoom: 15,
+		overviewMapControl: false,
+		panControl: false,
+		mapTypeId: google.maps.MapTypeId.SATELLITE
+	};
+	this.map = new google.maps.Map(this.elt, mapOptions);
+}
+/**
+ * The Player object for Ramble videos.
+ * @param {Element} elt The element where the player is to be rendered.
+ * @constructor
+ */
+function VideoPlayer(elt) {
+	this.currentTrack=null;
+	this.elt=elt;
+	this.EMPTY=true;
 
-		tracks[cT].richMarker.setPosition(new google.maps.LatLng(this.currentTrack.points[accuratePoint].latitude,this.currentTrack.points[accuratePoint].longitude));
-		currentPoint=accuratePoint;
-	});
-	document.getElementById('video_container').style.display="block";
-
+	this.elt.innerHTML="";
+	// Create Element Structure
+	// Container Div
+	var cont = document.createElement('div');
+	cont.setAttribute('class','video_container');
+	cont.setAttribute('id','video_container');
+	this.container=cont;
+	var topBar = document.createElement('div');
+	topBar.setAttribute('class','video-top-bar');
+	topBar.setAttribute('id','video-top-bar');
+	var closeButton = document.createElement('div');
+	closeButton.setAttribute('class','close-vid');
+	closeButton.setAttribute('id','close-vid');
+	var videoTitle = document.createElement('div');
+	videoTitle.setAttribute('class','video-title');
+	videoTitle.setAttribute('id','video-title');
+	this.videoTitle=videoTitle;
+	topBar.appendChild(closeButton);
+	topBar.appendChild(videoTitle);
+	var video = document.createElement('video');
+	video.setAttribute('class','video');
+	video.setAttribute('id','video');
+	this.video=video;
+	var videoControls = document.createElement('div');
+	videoControls.setAttribute('class','video-controls');
+	videoControls.setAttribute('id','video-controls');
+	var playPause = document.createElement('div');
+	playPause.setAttribute('class','play-pause');
+	playPause.setAttribute('id','play-pause');
+	this.playPause=playPause;
+	var scrubBar = document.createElement('div');
+	scrubBar.setAttribute('class','scrub-bar');
+	scrubBar.setAttribute('id','scrub-bar');
+	var loaded = document.createElement('div');
+	loaded.setAttribute('class','loaded');
+	loaded.setAttribute('id','loaded');
+	var played = document.createElement('div');
+	played.setAttribute('class','played');
+	played.setAttribute('id','played');
+	scrubBar.appendChild(loaded);
+	scrubBar.appendChild(played);
+	videoControls.appendChild(playPause);
+	videoControls.appendChild(scrubBar);
+	cont.appendChild(topBar);
+	cont.appendChild(video);
+	cont.appendChild(videoControls);
+	this.elt.appendChild(cont);
+}
+/**
+ * Sets the track to be viewed.
+ * @param {Track} track The track that is viewed in the video player.
+ */
+VideoPlayer.prototype.setTrack=function(track) {
+	this.currentTrack=track;
+	if(video.canPlayType("video/webm")) this.video.src="http://s3.amazonaws.com/ramble/"+this.currentTrack.name+"/"+this.currentTrack.name+".webm";
+	else if(video.canPlayType("video/mp4")) this.video.src="http://s3.amazonaws.com/ramble/"+this.currentTrack.name+"/"+this.currentTrack.name+".mp4";
+	else video.innerHTML = 'Sorry, your browser does not support HTML5 Video. Please try using a compatible browser. We recommend <a href="http://chrome.google.com">Google Chrome</a>.';
+	this.EMPTY=false;
+};
+/**
+ * Removes the current track from the viewer, restoring an empty state.
+ */
+VideoPlayer.prototype.clearTrack=function() {
+	this.currentTrack=null;
+	this.video.src=null;
+	this.container.style.display="none";
+	this.EMPTY=true;
+};
+/**
+ * Starts playing the video and the track.
+ */
+VideoPlayer.prototype.play=function() {
+	if(!this.EMPTY) {
+		this.video.play();
+	}
+};
+/**
+ * Pauses playback of the video and the track.
+ */
+VideoPlayer.prototype.pause=function() {
+	if(!this.EMPTY) {
+		this.video.pause();
+	}
+};
+/**
+ * Closes the video player on screen. Stops and resets playback if playing.
+ */
+VideoPlayer.prototype.close=function() {
+	if(!this.EMPTY) {
+		this.video.restart();
+	}
+};
+/**
+ * Opens the video player on screen.
+ */
+VideoPlayer.prototype.open=function() {
 
 };
+/**
+ * Unmutes and Shows the video player.
+ */
+VideoPlayer.prototype.show=function() {
+
+};
+/**
+ * Mutes and Hides the video player.
+ */
+VideoPlayer.prototype.hide=function() {
+
+};
+/**
+ * Pauses playback of the video and seeks to the beginning of the video.
+ */
+VideoPlayer.prototype.restart=function() {
+	if(!this.EMPTY) {
+		this.video.currentTime=0;
+		this.video.pause();
+	}
+};
+/**
+ * Move to a specific time in the video. The given time must be between 0 and the duration of the video.
+ * @param {number} time A specific time in the video.
+ */
+VideoPlayer.prototype.jumpTo=function(time) {
+
+};
+
+
+
